d33adabbc90dce3b6d8469273cdcb94df6144604
Jeet000001 <jeetpaul2222@gmail.com>
2026-08-23 23:12:38 +0530

    Redirect to HTML introduction page in concepts section

--- app/concepts/page.tsx
+++ app/concepts/page.tsx
@@ -1,14 +1,7 @@
-import React from "react";
+import { redirect } from "next/navigation";
 
 const Page = () => {
-  return (
-    <div className="w-full rounded-2xl min-h-[calc(100vh-8rem)] p-3 sm:min-h-[calc(100vh-8rem)] sm:p-4 md:min-h-[calc(100vh-7.5rem)] md:p-4 lg:h-[calc(100vh-7rem)] lg:p-3 lg:border border-white/10">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur sequi
-      vitae quidem eveniet velit aspernatur, ipsum voluptatem architecto quos,
-      minima nemo obcaecati dolorem, porro recusandae iure et accusamus? Cum,
-      tempore.
-    </div>
-  );
+  redirect("/concepts/html/introduction");
 };
 
 export default Page;
